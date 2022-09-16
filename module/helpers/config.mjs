8d1fd42e024843c6621f6d728dd1ffe9922c8e0f
Dachius <53961420+Dachius@users.noreply.github.com>
2022-09-16 16:50:33 -0500

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const DWN = {};
+
+// Define constants here, such as:
+DWN.foobar = {
+  'bas': 'DWN.bas',
+  'bar': 'DWN.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ DWN.abilities = {
+  "str": "DWN.AbilityStr",
+  "dex": "DWN.AbilityDex",
+  "con": "DWN.AbilityCon",
+  "int": "DWN.AbilityInt",
+  "wis": "DWN.AbilityWis",
+  "cha": "DWN.AbilityCha"
+};
+
+DWN.abilityAbbreviations = {
+  "str": "DWN.AbilityStrAbbr",
+  "dex": "DWN.AbilityDexAbbr",
+  "con": "DWN.AbilityConAbbr",
+  "int": "DWN.AbilityIntAbbr",
+  "wis": "DWN.AbilityWisAbbr",
+  "cha": "DWN.AbilityChaAbbr"
+};
